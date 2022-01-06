e4aee2122b6513abbf07841909ff239d5a6f6245
P Girinath <59465455+Giri2801@users.noreply.github.com>
2022-01-06 11:41:25 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from PIL import Image
 img_path = '81574683.png'
 ocr = get_ocr_data(img_path)
 updated_ocr = get_predicted_labels(ocr)
-updated_ocr = relie(img_path,ocr,'../../Hackathons/MTX/results/best_model/pytorch_model.bin')
+updated_ocr = relie(img_path,updated_ocr,'../../Hackathons/MTX/results/best_model/pytorch_model.bin')
 
 image = ocr_linking_visualization(img_path,updated_ocr,'simfang.ttf')
 image = Image.fromarray(image)
